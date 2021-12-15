3211fc1c8034a682bf45a4d6d4f4a433f202ceb4
smh1988-2 <seamhurley1988@gmail.com>
2021-12-15 09:56:27 -0800

    commit tree f9e44a27b120dfa842d7ea3cc65ef727b56950b0

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown num
+    while num >= 1
+        puts "#{num} SECOND(S)!"
+        sleep(1)
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+countdown 10
+
+def countdown_with_sleep num
+    while num >= 1
+        puts "#{num} SECOND(S)!"
+        sleep(1)
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep 10
